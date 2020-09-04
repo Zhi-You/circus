ffa5f08871466ef88e035f7b4ac018981078ed19
Zhi-You <tzyou98@gmail.com>
2020-09-04 17:24:21 +0800

    Add changes done in lecture

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,3 +1,10 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
@@ -10,16 +17,18 @@ public class Trainer {
         getToSpeak(a);
 
         Duck d2 = (Duck) a; // downcasting
+        getToSpeak(d2);
         train(new Duck());
-        // train(new Parrot());
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
+        train(new Parrot());
+        //circus.animal.Animal a2 = new circus.animal.Animal();
+        //circus.animal.Bird b2 = new circus.animal.Bird();
     }
 
     private static void getToSpeak(Animal animal) {
         System.out.println(animal.speak());
     }
 
+    // only available for duck objects
     private static void train(Bird bird) {
         Duck d = (Duck) bird;
         d.swim();
